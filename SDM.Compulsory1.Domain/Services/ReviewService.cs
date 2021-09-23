af3e1c2c723b09a162f67f13ae435fb1bfe2ff4f
Frann0 <50768854+Frann0@users.noreply.github.com>
2021-09-23 13:31:51 +0200

    9(ish) 10 og 11

--- SDM.Compulsory1.Domain/Services/ReviewService.cs
+++ SDM.Compulsory1.Domain/Services/ReviewService.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
 using SDM.Compulsory1.Core.IServices;
+using SDM.Compulsory1.Core.Models;
 using SDM.Compulsory1.Domain.IRepositories;
 
 namespace SDM.Compulsory1.Domain.Services
@@ -17,5 +20,37 @@ namespace SDM.Compulsory1.Domain.Services
         {
             throw new System.NotImplementedException();
         }
+        
+        public List<int> GetTopRatedMovies(int amount)
+                {
+                    throw new System.NotImplementedException();
+                }
+        
+                public List<int> GetTopMoviesByReviewer(int reviewer)
+                {
+                    List<Review> movies = _repo.GetAll().ToList().FindAll(r => r.Reviewer == reviewer);
+        
+                    var orderedMovies  = movies.OrderByDescending(m => m.Grade).ThenByDescending(m => m.ReviewDate).ToList();
+        
+                    List<int> movieIds = new List<int>();
+                    foreach (var m in orderedMovies)
+                    {
+                        movieIds.Add(m.Movie);
+                    }
+        
+                    return movieIds;
+                }
+        
+                public List<int> GetReviewersByMovie(int movie)
+                {
+                    List<int> reviewers = new List<int>();
+        
+                    foreach (var r in _repo.GetAll().ToList().FindAll(r => r.Movie == movie))
+                    {
+                        reviewers.Add(r.Reviewer);
+                    }
+        
+                    return reviewers;
+                }
     }
 }
