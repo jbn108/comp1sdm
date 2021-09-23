2ea83d3a74a591daf665733ec03a24584eee7ca0
Frann0 <50768854+Frann0@users.noreply.github.com>
2021-09-23 13:33:17 +0200

    UnitTests for 10 og 11

--- TestProductionCodeXunit/UnitTest1.cs
+++ TestProductionCodeXunit/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Moq;
 using SDM.Compulsory1.Core.Models;
 using SDM.Compulsory1.Domain.IRepositories;
@@ -20,6 +21,59 @@ namespace TestProductionCodeXunit
 
             int numberOfReviews = 0;
 
+        }
+        
+        [Fact]
+        public void TestForTopMovieByReviewer()
+        {
+            Mock<IReviewRepository> mock = new Mock<IReviewRepository>();
+
+            ReviewService mService = new ReviewService(mock.Object);
+
+            Review[] returnValue = { new Review {Reviewer = 1, Movie = 1, Grade = 1, ReviewDate = DateTime.Now.AddDays(-200)},
+                new Review {Reviewer = 1, Movie = 2, Grade = 5, ReviewDate = DateTime.Now.AddDays(-100)},
+                new Review {Reviewer = 1, Movie = 3, Grade = 2, ReviewDate = DateTime.Now.AddDays(-150)}, 
+                new Review {Reviewer = 2, Movie = 1, Grade = 5, ReviewDate = DateTime.Now.AddDays(-20)},
+                new Review {Reviewer = 1, Movie = 4, Grade = 2, ReviewDate = DateTime.Now.AddDays(-80)},
+                new Review {Reviewer = 3, Movie = 1, Grade = 1, ReviewDate = DateTime.Now.AddDays(-90)} 
+            };
+
+            mock.Setup(m => m.GetAll()).Returns(() => returnValue);
+
+            List<int> actualResult = mService.GetTopMoviesByReviewer(1);
+
+            mock.Verify(m => m.GetAll(), Times.Once);
+
+            Assert.True(actualResult.Count == 4);
+            Assert.Collection(actualResult,
+                item => Assert.Equal(2, item),
+                item => Assert.Equal(4, item),
+                item => Assert.Equal(3, item),
+                item => Assert.Equal(1,item)
+            );
+        }
+        
+        [Fact]
+        public void TestForReviewersOnGivenMovie()
+        {
+            Mock<IReviewRepository> mock = new Mock<IReviewRepository>();
+
+            ReviewService mService = new ReviewService(mock.Object);
+
+            Review[] returnValue = { new Review {Reviewer = 1, Movie = 1},
+                new Review {Reviewer = 3, Movie = 1},
+                new Review {Reviewer = 2, Movie = 2} 
+            };
+
+            mock.Setup(m => m.GetAll()).Returns(() => returnValue);
+
+            List<int> actualResult = mService.GetReviewersByMovie(1);
+
+            mock.Verify(m => m.GetAll(), Times.Once);
+
+            Assert.True(actualResult.Count == 2);
+            Assert.Contains(returnValue[0].Reviewer, actualResult);
+            Assert.Contains(returnValue[1].Reviewer, actualResult);
         }
     }
 }
