483591f217c5bfe6b6bf4a436ca1af1dc5fb05cf
Jonas <jbuus9@gmail.com>
2021-09-24 08:43:12 +0200

    work

--- SDM.Compulsory1.Domain/Services/ReviewService.cs
+++ SDM.Compulsory1.Domain/Services/ReviewService.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
 using SDM.Compulsory1.Core.IServices;
+using SDM.Compulsory1.Core.Models;
 using SDM.Compulsory1.Domain.IRepositories;
 
 namespace SDM.Compulsory1.Domain.Services
@@ -25,6 +28,98 @@ namespace SDM.Compulsory1.Domain.Services
             }
 
             return numberOfReviews;
+        }
+
+        public double GetAverageRateFromReviewer(int reviewer)
+        {
+            int sum = 0;
+            int numberOfReviews = 0;
+            
+            foreach (var r in _repo.GetAll().Where(r => r.Reviewer == reviewer).ToList())
+            {
+                sum += r.Grade;
+                numberOfReviews++;
+            }
+
+            return sum / numberOfReviews;
+        }
+
+        public int GetNumberOfRatesByReviewer(int reviewer, int rate)
+        {
+            int numberOfRates = 0;
+            foreach (var r in _repo.GetAll().Where(r => r.Reviewer == reviewer))
+            {
+                if (r.Grade == rate)
+                {
+                    numberOfRates++;
+                }
+            }
+            return numberOfRates;
+        }
+
+        public int GetNumberOfReviews(int movie)
+        {
+            return _repo.GetAll().Where(r => r.Movie == movie).ToList().Count;
+        }
+
+        public double GetAverageRateOfMovie(int movie)
+        {
+            int sum = 0;
+            int numberOfReviews = 0;
+            
+            foreach (var r in _repo.GetAll().Where(r => r.Movie == movie).ToList())
+            {
+                sum += r.Grade;
+                numberOfReviews++;
+            }
+
+            return sum / numberOfReviews;
+        }
+
+        public int GetNumberOfRates(int movie, int rate)
+        {
+            int numberOfRates = 0;
+            foreach (var r in _repo.GetAll().Where(r => r.Movie == movie))
+            {
+                if (r.Grade == rate)
+                {
+                    numberOfRates++;
+                }
+            }
+            return numberOfRates;
+        }
+
+        public List<int> GetMoviesWithHighestNumberOfTopRates()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<int> GetMostProductiveReviewers()
+        {
+            Dictionary<int, int> topReviewers = new Dictionary<int, int>();
+            
+            foreach (var r in _repo.GetAll())
+            {
+                if (!topReviewers.ContainsKey(r.Reviewer))
+                {
+                    topReviewers.Add(r.Reviewer, 1);
+                }
+                else
+                {
+                    topReviewers[r.Reviewer] += 1;
+                }
+            }
+
+            List<KeyValuePair<int, int>> revs = topReviewers.OrderByDescending(r => r.Value).ToList();
+            List<int> final = new List<int>();
+            
+            final.Add(revs[0].Key);
+            final.Add(revs[1].Key);
+            final.Add(revs[2].Key);
+            final.Add(revs[3].Key);
+            final.Add(revs[4].Key);
+
+            return final;
         }
     }
 }
--- TestProductionCodeXunit/UnitTest1.cs
+++ TestProductionCodeXunit/UnitTest1.cs
@@ -12,21 +12,22 @@ namespace TestProductionCodeXunit
     public class UnitTest1
     {
         // Test: GetNumberOfReviewsForReviewer()
-        
+
         [Fact]
         public void TestForNumberOfReviews()
         {
             //Arrange
             Mock<IReviewRepository> m = new Mock<IReviewRepository>();
 
-            Review[] returnValue = { 
-                new Review() { Reviewer = 1, Grade = 2, Movie = 1, ReviewDate = DateTime.Now},
-                new Review() { Reviewer = 1, Grade = 2, Movie = 2, ReviewDate = DateTime.Now},
-                new Review() { Reviewer = 1, Grade = 2, Movie = 3, ReviewDate = DateTime.Now},
-                new Review() { Reviewer = 1, Grade = 2, Movie = 4, ReviewDate = DateTime.Now},
-                new Review() { Reviewer = 1, Grade = 2, Movie = 5, ReviewDate = DateTime.Now},
-                new Review() { Reviewer = 1, Grade = 2, Movie = 6, ReviewDate = DateTime.Now},
-                new Review() { Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now}
+            Review[] returnValue =
+            {
+                new Review() {Reviewer = 1, Grade = 2, Movie = 1, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 2, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 3, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 4, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 5, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 6, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now}
             };
             m.Setup(m => m.GetAll()).Returns(() => returnValue);
 
@@ -40,6 +41,179 @@ namespace TestProductionCodeXunit
 
             Assert.True(actualResult == returnValue.Length);
 
+        }
+
+        
+        // GetAverageRateFromReviewer()
+        [Fact]
+        public void TestForAverageRateFromReviewer()
+        {
+            //Arrange
+            Mock<IReviewRepository> m = new Mock<IReviewRepository>();
+
+            Review[] returnValue =
+            {
+                new Review() {Reviewer = 1, Grade = 2, Movie = 1, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 2, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 3, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 4, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 5, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 6, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now}
+            };
+            m.Setup(m => m.GetAll()).Returns(() => returnValue);
+
+            ReviewService mService = new ReviewService(m.Object);
+
+            //Act
+            double actualResult = mService.GetAverageRateFromReviewer(1);
+
+            //Assert
+            m.Verify(m => m.GetAll(), Times.Once);
+
+            Assert.True(actualResult == 2);
+        }
+
+        // GetNumberOfRatesByReviewer
+
+        [Fact]
+        public void TestForNumberOfRatesFromReviewer()
+        {
+            //Arrange
+            Mock<IReviewRepository> m = new Mock<IReviewRepository>();
+
+            Review[] returnValue =
+            {
+                new Review() {Reviewer = 1, Grade = 2, Movie = 1, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 2, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 3, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 4, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 5, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 6, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now}
+            };
+            m.Setup(m => m.GetAll()).Returns(() => returnValue);
+
+            ReviewService mService = new ReviewService(m.Object);
+
+            //Act
+            double actualResult = mService.GetNumberOfRatesByReviewer(1, 2);
+
+            //Assert
+            m.Verify(m => m.GetAll(), Times.Once);
+
+            Assert.True(actualResult == 7);
+        }
+        
+        
+        // GetNumberOfReviews
+        
+        [Fact]
+        public void GetNumberOfReviews()
+        {
+            //Arrange
+            Mock<IReviewRepository> m = new Mock<IReviewRepository>();
+
+            Review[] returnValue =
+            {
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now}
+            };
+            m.Setup(m => m.GetAll()).Returns(() => returnValue);
+
+            ReviewService mService = new ReviewService(m.Object);
+
+            //Act
+            double actualResult = mService.GetNumberOfReviews(7);
+
+            //Assert
+            m.Verify(m => m.GetAll(), Times.Once);
+
+            Assert.True(actualResult == 7);
+        }
+
+        // GetAverageRateOfMovie
+        
+        [Fact]
+        public void GetAverageRateOfMovie()
+        {
+            //Arrange
+            Mock<IReviewRepository> m = new Mock<IReviewRepository>();
+
+            Review[] returnValue =
+            {
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now}
+            };
+            m.Setup(m => m.GetAll()).Returns(() => returnValue);
+
+            ReviewService mService = new ReviewService(m.Object);
+
+            //Act
+            double actualResult = mService.GetAverageRateOfMovie(7);
+
+            //Assert
+            m.Verify(m => m.GetAll(), Times.Once);
+
+            Assert.True(actualResult == 2);
+        }
+        
+        // GetNumberOfRates
+        
+        [Fact]
+        public void GetNumberOfRates()
+        {
+            //Arrange
+            Mock<IReviewRepository> m = new Mock<IReviewRepository>();
+
+            Review[] returnValue =
+            {
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now},
+                new Review() {Reviewer = 1, Grade = 2, Movie = 7, ReviewDate = DateTime.Now}
+            };
+            m.Setup(m => m.GetAll()).Returns(() => returnValue);
+
+            ReviewService mService = new ReviewService(m.Object);
+
+            //Act
+            int actualResult = mService.GetNumberOfRates(7, 2);
+
+            //Assert
+            m.Verify(m => m.GetAll(), Times.Once);
+            
+            Assert.True(actualResult == 7);
+        }
+        
+        // GetMoviesWithHighestNumberOfTopRates
+        
+        [Fact]
+        public void GetMoviesWithHighestNumberOfTopRates()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // GetMostProductiveReviewers
+
+        [Fact]
+        public void GetMostProductiveReviewers()
+        {
+
+            throw new System.NotImplementedException();
         }
     }
 }
